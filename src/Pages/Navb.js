d363ff606edfeade541fa4a0ddb91ac545eb1bd8
jiylee <jjenny4711@gmail.com>
2023-06-08 16:10:24 -0400

    ff

--- src/Pages/Navb.js
+++ src/Pages/Navb.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import "../App.css";
+import "../CSS/Navb.css";
 import { useDispatch, useSelector } from "react-redux";
 import { gptAction } from "../redux/actions/msgAction";
 import { useNavigate } from "react-router-dom";
